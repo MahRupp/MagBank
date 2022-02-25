41868c809b0191a2231b3570501720437996168e
MAh <mahrupp@gmail.com>
2022-02-25 12:05:43 -0300

    finalizando credit card section

--- src/Components/CreditCard.js
+++ src/Components/CreditCard.js
@@ -1,25 +1,83 @@
 import React from 'react';
-import { Container , Button, Row, Col, Image} from 'react-bootstrap';
+import { Container , Button, Row, Col, Image } from 'react-bootstrap';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreditCard,
+   faMobile, 
+   faShoppingCart, 
+   faWallet } from "@fortawesome/free-solid-svg-icons";
 import Cartao from '../Assets/credit-card.jpg';
 import './CreditCard.scss';
 
 
+
 const CreditCard = () => (
-  <Container className='my-lg-5'>
+  <Container className= 'my-lg-5'>
     <Row>
-      <Col xs={12} lg={7} className='mb-5 mb-lg-0 px-5 pc-lg-0'>
-        <row> 
-          <h2 className='h2 display-4 my-5'>Cartão de Crédito</h2>
-        </row>
-        <row>
-          <p>Crédito Pessoal</p> 
-          <p>Pagamentos on-line</p>
-          <p>APP</p>
-          <p>Carteira Digital</p>
-        </row>
-       
+      <Col xs={12} lg={7} className='mb-5 mb-lg-0 px-5 px-lg-0'>
+        <Row> 
+          <h2 className='h2 display-4 fw-normal my-5 '>Cartão de Crédito</h2>
+        </Row>
+        <Row>
+          <Col xs={12} lg={6} className='mb-4'>
+            <Row> 
+              <Col xs={2} className='d-flex justify-content-center'>
+                <FontAwesomeIcon
+                icon={faCreditCard}
+                size='2x'
+                color='#f05656'/>
+              </Col>
+              <Col xs={10} className='h5 text-muted'>
+                Crédito Pessoal
+              </Col>
+            </Row>            
+          </Col>
+
+          <Col xs={12} lg={6} className='mb-4'>
+            <Row> 
+              <Col xs={2} className='d-flex justify-content-center'>
+                <FontAwesomeIcon
+                  icon={faShoppingCart}
+                  size='2x'
+                  color='#f05656'/>
+              </Col>
+              <Col xs={10} className='h5 text-muted'>
+               Pagamentos Online
+              </Col>
+            </Row>            
+          </Col>
+
+          <Col xs={12} lg={6} className='mb-4'>
+            <Row> 
+              <Col xs={2} className='d-flex justify-content-center'>
+                <FontAwesomeIcon
+                  icon={faMobile}
+                  size='2x'
+                  color='#f05656'/>
+              </Col>
+              <Col xs={10} className='h5 text-muted'>
+                APP
+              </Col>
+            </Row>            
+          </Col>
+
+          <Col xs={12} lg={6} className='mb-4'>
+            <Row> 
+              <Col xs={2} className='d-flex justify-content-center'>
+                <FontAwesomeIcon
+                  icon={faWallet}
+                  size='2x'
+                  color='#f05656'/>
+              </Col>
+              <Col xs={10} className='h5 text-muted'>
+                Carteira Digital
+              </Col>
+            </Row>            
+          </Col>
+          
+        </Row>       
       </Col>
-      <Col className='text-lg-start my-5'>
+
+      <Col xs={12} lg={5} className='text-lg-start'>
         <Image src= {Cartao} alt='Magbank Credit Card Example' fluid/> 
         <Button variant="outline-light">Abra sua conta</Button>
       </Col>
